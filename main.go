bf1405720d48c5dc7b1a3483b404c4f2ad641c95
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-07-09 20:13:55 +0300

    Updated main to assemble various parts together.

--- main.go
+++ main.go
@@ -2,10 +2,36 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/akornatskyy/scheduler/core"
+	"github.com/akornatskyy/scheduler/infrastructure/http"
+	"github.com/akornatskyy/scheduler/infrastructure/postgres"
 )
 
 func main() {
 	log.Printf("starting...")
+
+	dsn := os.Getenv("DSN")
+	service := &core.Service{
+		Repository: postgres.NewRepository(dsn),
+	}
+
+	server := &http.Server{
+		Service: service,
+	}
+
+	server.Start()
+
+	c := make(chan os.Signal)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	<-c
+
+	log.Println("shutting down...")
+
+	server.Stop()
 
 	log.Println("done")
 }
