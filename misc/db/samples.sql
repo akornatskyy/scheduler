d1b4cc7d04533cc0d22ec24d03923fbdc8b49509
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-07-12 20:18:26 +0300

    Added random samples for collections table.

---
+++ misc/db/samples.sql
@@ -0,0 +1,12 @@
+INSERT INTO collection (id,"name",updated,state_id) VALUES
+('6c48f4f5-9f88-438b-86c2-f8df19d1b782','nisi ut aliquip ex ea commodo co','2006-01-27 10:13:03.958',2)
+,('1c706c6c-b82b-4c7d-8909-471e3434fca4','sed do eiusmod tempor incididu','1990-06-25 08:50:38.668',2)
+,('59ffdf80-1f44-4969-9083-8aa4e6bd3fa0','mollit anim id est laborum.Lorem ipsum dolor sit amet, consecte','1996-01-16 03:31:06.598',1)
+,('7cac2c9a-00c2-4a3a-9d93-6e2b2026b890','Excepteur sint occaecat cupidatat non proident, sunt in culpa ','2006-03-14 15:05:48.093',1)
+,('a323fc62-1490-4d91-90d9-e086a11d70e4','sunt in culp','1986-03-13 23:33:34.119',2)
+,('caa4b2fd-c1fd-46e1-be86-8411642eb559','ad minim veniam, quis nostrud exercitation ullamco laboris nisi ','2005-04-30 08:51:29.798',1)
+,('37764a5f-0a63-47ae-9aff-655098eb4163','sit amet, consectetur adipiscing elit, sed do eiusmod tempor in','1988-02-25 11:18:11.833',2)
+,('82bf52ec-f0b8-4748-b822-c14ee32aee62','incididunt ut labore et dolore magna aliqua. Ut enim ad mini','2004-07-30 12:04:27.372',1)
+,('9bd9eca8-6389-409f-9d5e-9c6186475a28','in voluptate velit esse cillum dolore eu fugia','1985-05-27 21:15:38.369',1)
+,('0a69fe57-37f5-4698-ac73-45cc0285c5e7','velit esse cillum dolore eu fugi','1985-11-07 13:49:51.474',2)
+;
