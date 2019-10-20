1ec65b3fa9effd84cdf07ef0a278e36d73a660a0
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-10-20 20:05:10 +0300

    Added http caching directives while serving a static content.

--- infrastructure/http/routes.go
+++ infrastructure/http/routes.go
@@ -29,9 +29,9 @@ func (s *Server) Routes() http.Handler {
 
 	r.HandlerFunc("GET", "/health", s.health())
 
-	r.Handle("GET", "/", serveFile("static/index.html"))
-	r.Handle("GET", "/favicon.ico", serveFile("static/favicon.ico"))
-	r.Handle("GET", "/js/*filepath", serveFiles(http.Dir("static/js")))
+	r.Handle("GET", "/", serveIndex())
+	r.Handle("GET", "/favicon.ico", serveFavicon())
+	r.Handle("GET", "/js/*filepath", serveJavascript())
 
 	return r
 }
--- infrastructure/http/ui.go
+++ infrastructure/http/ui.go
@@ -7,16 +7,29 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func serveFile(name string) httprouter.Handle {
+func serveIndex() httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		http.ServeFile(w, r, name)
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("Cache-Control", "max-age=180")
+		http.ServeFile(w, r, "static/index.html")
 	}
 }
 
-func serveFiles(root http.FileSystem) httprouter.Handle {
-	fileServer := gziphandler.GzipHandler(http.FileServer(root))
+func serveFavicon() httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("Cache-Control", "max-age=180, immutable")
+		http.ServeFile(w, r, "static/favicon.ico")
+	}
+}
+
+func serveJavascript() httprouter.Handle {
+	fileServer := gziphandler.GzipHandler(http.FileServer(http.Dir("static/js")))
 	return func(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
 		req.URL.Path = p.ByName("filepath")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("Cache-Control", "max-age=31536000, immutable")
+		w.Header().Set("Content-Type", "application/javascript; charset=UTF-8")
 		fileServer.ServeHTTP(w, req)
 	}
 }
