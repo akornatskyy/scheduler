4cb1558adb0973117472fa096f6ebbfda5226976
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2020-09-29 20:02:49 +0300

    Fixed header position.

--- ui/shared/components/Header.js
+++ ui/shared/components/Header.js
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import {Link} from 'react-router-dom';
 
 const Header = () => (
-  <Navbar>
+  <Navbar className="mb-4 pl-0">
     <Navbar.Brand>Scheduler</Navbar.Brand>
     <Nav>
       <Nav.Link as={Link} to="/collections">
